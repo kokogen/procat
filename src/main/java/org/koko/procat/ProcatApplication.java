b47511c4363f4bbc76bf9b6c8ba0d2f0c455b0de
koko <kokogen@gmail.com>
2023-01-05 23:29:55 +0400

    Init commit

---
+++ src/main/java/org/koko/procat/ProcatApplication.java
@@ -0,0 +1,13 @@
+package org.koko.procat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcatApplication.class, args);
+	}
+
+}
